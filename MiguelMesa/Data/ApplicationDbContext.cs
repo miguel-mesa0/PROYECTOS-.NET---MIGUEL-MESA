4cbb33e3a4e7b608e6881e0c819ab74b307d5d3f
Miguel Mesa <miguelmesa1840@gmail.com>
2025-11-13 00:08:50 -0500

    Subo carpeta grande con proyectos

---
+++ MiguelMesa/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiguelMesa.Models;
+
+namespace MiguelMesa.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Cliente> Clientes { get; set; }
+		public DbSet<Producto> Productos { get; set; }
+		public DbSet<Venta> Ventas { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Producto>()
+				.Property(p => p.Precio)
+				.HasColumnType("decimal(18,2)");
+
+			modelBuilder.Entity<Venta>()
+				.HasOne(v => v.Cliente)
+				.WithMany(c => c.Ventas)
+				.HasForeignKey(v => v.ClienteId);
+
+			modelBuilder.Entity<Venta>()
+				.HasOne(v => v.Producto)
+				.WithMany(p => p.Ventas)
+				.HasForeignKey(v => v.ProductoId);
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ MiguelMesa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiguelMesa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
